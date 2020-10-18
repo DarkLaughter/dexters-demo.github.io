dbf346978261169e86edd05104983cc9b459a44e
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-10-17 21:45:05 -0400

    base components

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Squad  from '../images/squad.jpg'
+
+function Home() {
+    return (
+        <div>
+            <h1>I am the home page</h1>
+            <img src={Squad}/>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <ul>home</ul>
+                <ul>Mandark</ul>
+                <ul>Dee Dee</ul>
+
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default Navbar
