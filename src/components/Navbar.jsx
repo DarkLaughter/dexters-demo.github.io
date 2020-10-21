ef6d0be2760f8c6ed0d411d4e81e21fbfae9cb94
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-10-21 11:25:20 -0400

    css updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div >
+        <div className="navBar" >
             <ul className="navBar" >
                 <li>home</li>
                 <li>Mandark</li>
